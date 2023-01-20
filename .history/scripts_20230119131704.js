998a5b47a33d4235562c2fbb4bc45f44230a93bd
Oladeji Abdullah <oladejiabdullah17@gmail.com>
2023-01-20 06:12:55 +0100

    Hamburger opening error

---
+++ .history/scripts_20230119131704.js
@@ -0,0 +1,44 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
+
+// Hamburger Menu
+const showMenu = () => {
+    var x = document.getElementById("mobi-links")
+    var a = document.getElementById("close_hamburger")
+    var b = document.getElementById("hamburger")
+    if (x.style.display === "flex") {
+        x.style.display = 'none'
+        a.style.display = 'none'
+        b.style.display = 'block'
+    } else {
+        x.style.display = 'flex'
+        a.style.display = 'block'
+        b.style.display = 'none'
+    }
+}
+
+
+const closeHamburger = () => {
+    var a = document.getElementById("close_hamburger")
+    var x = document.getElementById("mobi-links")
+    var b = document.getElementById("hamburger")
+
+    if (x.style.display === "flex") {
+        //x.style.display = "none"
+        x.classList.add("after")
+        a.style.display = "none"
+        b.style.display = "block"
+    }
+}
