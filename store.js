ebcb63c8d8f9afcfec290d7c709e23af87ab3a3b
Oladeji Abdullah <oladejiabdullah17@gmail.com>
2023-01-25 21:57:57 +0100

    Store section update

---
+++ store.js
@@ -0,0 +1,25 @@
+// Changing store section
+const slide_page = document.getElementById("slides");
+
+const shoe_page = document.getElementById("shoes");
+
+const belt_page = document.getElementById("belts")
+
+
+const slidesPane = () => {
+    slide_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const shoesPane = () => {
+    shoe_page.style.display = "grid";
+    slide_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const beltsPane = () => {
+    belt_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    slide_page.style.display = "none";
+}
