0d5c48d20ae743c53d47b14d5bb29ed9f970c4cc
Oladeji Abdullah <oladejiabdullah17@gmail.com>
2023-02-21 21:59:32 +0100

    product Pop up  and responsiveness

---
+++ .history/store_20230221213138.js
@@ -0,0 +1,37 @@
+// Changing store section
+const slide_page = document.getElementById("slides");
+
+const shoe_page = document.getElementById("shoes");
+
+const belt_page = document.getElementById("belts")
+
+
+const slidesPane = () => {
+    slide_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const shoesPane = () => {
+    shoe_page.style.display = "grid";
+    slide_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const beltsPane = () => {
+    belt_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    slide_page.style.display = "none";
+}
+
+// Product Pop-Up
+const productPop = () => {
+    var product = document.getElementById("productPage")
+
+    if (product.style.display === "none") {
+        product.style.display === "block"
+    } 
+    // else {
+    //     productCard.style.display === "none"
+    // }
+}
---
+++ .history/store_20230221213858.js
@@ -0,0 +1,39 @@
+// Changing store section
+const slide_page = document.getElementById("slides");
+
+const shoe_page = document.getElementById("shoes");
+
+const belt_page = document.getElementById("belts")
+
+
+const slidesPane = () => {
+    slide_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const shoesPane = () => {
+    shoe_page.style.display = "grid";
+    slide_page.style.display = "none";
+    belt_page.style.display = "none";
+}
+
+const beltsPane = () => {
+    belt_page.style.display = "grid";
+    shoe_page.style.display = "none";
+    slide_page.style.display = "none";
+}
+
+// Product Pop-Up
+const productPop = () => {
+    var product = document.getElementById("productPage")
+
+    if (product.style.display === "block") {
+        product.style.display === "none"
+    } 
+    // else {
+    //     product.style.display === "block"
+    // }
+}
+
+const closeProductPop 
