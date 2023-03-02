de6f4d3957bea423231affa4bfc7ca056238d3a3
Oladeji Abdullah <oladejiabdullah17@gmail.com>
2023-03-02 23:29:11 +0100

    Alteration of design

---
+++ .history/store_20230302231207.js
@@ -0,0 +1,89 @@
+// Product Pop-Up
+const productPop = () => {
+    document.getElementById("productPage").style.display = "block"
+}
+
+const closeProductPop = () => {
+    document.getElementById("productPage").style.display = "none"
+}
+
+// Fetching store images
+fetch("storeImages.json")
+    .then(response => response.json())
+    .then(data => {
+        let imageDisplay = document.querySelector(".items");
+        let pics = "";
+
+        for (let product of data) {
+            pics += `
+            <figure id="productOne" class='good ${product.class}'>
+                <img src='${product.img}' alt="" class="img_class" onclick="productPop()">
+
+                <div class="product_info">
+                    <h4 class="pdt_name">${product.storeInfo.name}</h4>
+                    <p class="pdt_desc">${product.storeInfo.Desc}</p>
+                
+                    <div class="contact_row">
+                        <small class="pdt_price">${product.storeInfo.price}</small>
+
+                        <small><strike>${product.storeInfo.priceOff}</strike></small>
+                    </div>
+                </div>
+            </figure>
+        `;
+        }
+        imageDisplay.innerHTML = pics;
+    });
+
+// Fetching each product info
+// fetch("store.json")
+// .then(response => response.json())
+// .then(data =>{
+//     let productDisplay = document.querySelector(".product_card"); 
+//     let out = "";
+
+//     for(let product of data){
+//         out += `
+//                 <div class="close_product_card" onclick="closeProductPop()">
+//                     <h1>X</h1>
+//                 </div>
+
+//                 <img src='${product.img}' alt="" class="img_class">
+    
+//                 <div class="product_info" key="${product.id}">
+//                     <h4 class="pdt_name">${product.storeInfo.name}</h4>
+
+//                     <p class="pdt_desc">${product.storeInfo.Desc}</p>
+                    
+//                     <div class="contact_row">
+//                         <small class="pdt_price">${product.storeInfo.price}</small>
+//                         <button>Direct Order</button>
+//                     </div>
+//                 </div>
+//         `;
+//     }
+//     productDisplay.innerHTML = out;
+// });
+
+// Filter
+const filterObjects = (filter = "good") => {
+    let images = document.querySelectorAll(".good");
+    for (let i = 0; i < images.length; i++) {
+        const classNames = images[i].className;
+        images[i].style.display = classNames.includes(filter) ? "grid" : "none";
+    }
+};
+
+filterObjects();
+
+// FOR..OF
+    // for (const image of images) {
+    //     const classNames = image.className;
+    //     image.style.display = classNames.includes(filter) ? "grid" : "none";
+    // }
+
+    // FOR EACH
+    // images.forEach(image => {
+    //     const classNames = image.className;
+    //     image.style.display = classNames.includes(filter) ? "grid" : "none";
+    // });
